f91a689b8e353cf48f2116706d804c50de00f105
Cody Ley-Han <codyleyhan@gmail.com>
2016-12-20 00:02:42 -0800

    added in an example middleware

--- app/auth/controller.py
+++ app/auth/controller.py
@@ -3,20 +3,30 @@ from werkzeug import check_password_hash, generate_password_hash
 
 from app import db
 from app.auth.models.User import User
-
+from app.auth.decorators import requires_login
 
 
 auth = Blueprint('auth', __name__, url_prefix='/auth')
 
+# This is middleware that happens before the route controller is executed
+# a user makes a request -> before_request(request) -> to specif route -> response
+@auth.before_request
+def before_request():
+    g.user = {
+        'name' : 'test',
+        'username' : 'testing'
+    }
+
 @auth.route('/me/')
 def home():
+    print(g.get('user', None))
     return jsonify({'user' : 'test'})
 
 @auth.route('/')
 def index():
     user = User.query.get(1)
     print(user.toDict())
-
+    print(request.headers)
     return jsonify({'message': 'done', 'user': user.toDict()})
 
 @auth.route('/register/', methods=['GET', 'POST'])
--- app/auth/decorators.py
+++ app/auth/decorators.py
@@ -2,10 +2,8 @@ from functools import wraps
 from flask import g, url_for, redirect,request
 
 
-def requries_login(f):
+def requires_login(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if g.user is None:
-            return redirect(url_for('auth.login', next=request.path))
         return f(*args, **kwargs)
     return decorated_function
