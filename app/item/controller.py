9dae93050746c23344de054e5a53f4b3f84cc523
WhiteRabbit27 <RKhillah@mail.ccsf.edu>
2016-12-22 11:51:40 -0500

    Item and controller started.

--- app/item/controller.py
+++ app/item/controller.py
@@ -1,4 +1,17 @@
 from flask import Blueprint, request, g, redirect, url_for, jsonify
 from datetime import datetime, timedelta
+import pprint
 
+from app.auth.models.User import User
 from app.auth.decorators import requires_login, requres_status_manager
+
+#@{what}.route('/add_item_to_db', methods=['POST'])
+@requires_login
+@requres_status_manager
+def add_item_to_db():
+    #depth=5? what does this do?
+    print(pprint.pformat(request.values, depth=5))
+
+    # Check whether item exists
+    item = Item.query.fileter(Item.name==request.json['name']).all()
+    
---
+++ app/item/models/Item.py
@@ -0,0 +1,23 @@
+from app import db
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    cost = db.Column(db.Float)
+    description = db.
+
+    def __init__(self, name, cost, description):
+        self.name = name
+        self.cost = cost
+        self.description = description
+
+    def to_dict(self):
+        item = {
+            'id' = self.id,
+            'name' = self.name,
+            'cost' = self.cost
+            'description' = self.description
+        }
+
+    def __repr__(self):
+        return 'id: %d \tname: %s \tcost: %f' % (self.id, self.name, self.cost)
