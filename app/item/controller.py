0e7db89e80faba71501f0836ddc874fece93df38
WhiteRabbit27 <RKhillah@mail.ccsf.edu>
2016-12-23 23:33:56 -0500

    get and dict not working

--- app/item/controller.py
+++ app/item/controller.py
@@ -21,6 +21,7 @@ def index():
             'message': 'in index'
         }), 201
 
+    # add item to database
     if request.method == 'POST':
         print("got into add_item()")
         form = ItemValidator(data=request.json)
@@ -44,49 +45,52 @@ def index():
             }), 201
         return jsonify({'message': 'There was an error adding data', 'error': form.errors}), 400
 
-## view
 @itemMod.route('/<string:itemName>', methods=['GET', 'PUT', 'DELETE'])
-def view(itemName):
-    if request.method == 'GET': pass
-    if request.method == 'PUT': pass
-    if request.method == 'DELETE': pass
+def update(itemName):
+    #get the Item.
+    item = get(itemName)
+    print(item)
+    if not item:
+        return jsonify('%r not found in database.' % itemName)
 
+    # view a single Item
+    if request.method == 'GET':
+        item = dict(item)
+        return jsonify({
+            'item': item
+        })
+
+    # update a single Item
+    if request.method == 'PUT':
+        pass
+
+    # delete a single item
+    if request.method == 'DELETE':
+        try:
+            db.session.delete(item)
+            db.session.commit()
+            return jsonify('%r deleted from the database.' % itemName)
+        except:
+            return jsonify(item)
+
+## helper function
+def get(itemName):
+    """
     if itemName == "":
         return jsonify('error: itemName must not be a null string argument'), 404
-
+    """
     item = Item.query.filter(Item.name == itemName).all()
-
     if not item:
-        return jsonify({
-            'Item was not found. Ensure you have the right spelling.',
-            '%r not found in database.' % itemName
-        }), 404
+        return False
+    #item = item[0].to_dict()
+    return item
 
-    item = item[0].to_dict()
+def dict(item):
+    return item[0].to_dict()
+
+    """
     return jsonify({
         'message': 'inside view!',
         'item': item
     }), 201
-
-
-## update
-@itemMod.route('/update', methods=['POST'])
-#itemMod.route('/<int: itemId>', methods=['GET', 'PUT', 'DELETE'])
-def update():
-    return jsonify({
-        'message': 'Yes! inside za update side \'o things'
-    })
-
-## get
-@itemMod.route('/get', methods=['GET'])
-def get():
-    return jsonify({
-        'message': 'Yup Yup! 1M/yr here we come!'
-    })
-
-##delete item
-@itemMod.route("/delete")
-def delete():
-    return jsonify({
-        'message': 'oh my my! we\'re deleting something??'
-    })
+    """
