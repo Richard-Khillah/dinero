03e2348e024e97102357ea7efd506b9d1edbb69c
Cody Ley-Han <codyleyhan@gmail.com>
2016-12-27 23:26:30 -0800

    added route scaffold

--- app/bill/controller.py
+++ app/bill/controller.py
@@ -1,10 +1,18 @@
 from flask import Blueprint, request, g, jsonify
 
 from app import app, db
+from app.auth.decorators import requires_login
+
 
 billMod = Blueprint('bills', __name__, url_prefix='/bills')
 
 
 @billMod.route('/')
+@requires_login
 def bill_index():
-    
+    pass
+
+@billMod.route('/<int:billId>')
+@requires_login
+def bill_single(billId):
+    pass
