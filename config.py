936a14eae0df50d0d707f0c3ae692a6a733a6ab3
Cody Ley-Han <codyleyhan@gmail.com>
2017-01-04 21:27:52 -0800

    added in command line params for port and hostname

--- config.py
+++ config.py
@@ -5,13 +5,13 @@ _basedir = os.path.abspath(os.path.dirname(__file__))  #+ '/data/'
 #wrt to +'/data', we might be able to @app.route
 
 class Default:
-    PORT = 5050
+    PORT = 5000
 
 class Development(Default):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(_basedir, 'app.sqlite')
     SQLALCHEMY_MIGRATE_REPO = os.path.join(_basedir, 'db_repository')
     DEBUG = True
-    SECRET_KEY = 'asdfljk23piorj9032rjidafjd90sfasfds0fiasd'
+    SECRET_KEY = 'secret'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class Testing(Development):
--- run.py
+++ run.py
@@ -1,4 +1,18 @@
+from optparse import OptionParser
 from app import app
 
+parser = OptionParser()
+parser.add_option("-p", "--port", dest="user_port", help="port to run server on", metavar="PORT")
+parser.add_option("-u", "--host", dest="user_host", help="host to run on ", metavar="HOST")
 
-app.run(debug=True)
+(options, args) = parser.parse_args()
+
+user_port = 5000
+user_host = '127.0.0.1'
+
+if options.user_port != None:
+    user_port = int(options.user_port)
+if options.user_host != None:
+    user_host = options.user_host
+
+app.run(debug=True, port=user_port, host=user_host)
